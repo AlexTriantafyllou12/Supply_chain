a8d7bc47405e9fe9c53dda469ba5bd5d5c1e40fb
lina-rietuma <141722035+lina-rietuma@users.noreply.github.com>
2023-08-11 12:14:24 +0100

    Removing generate_demand() function to move development to feature branch

--- funcs.py
+++ funcs.py
@@ -1,16 +1,1 @@
-import pandas as pd
-from datetime import datetime
 
-
-def generate_demand(
-        start_date = "2023-08-01",
-        time_periods = 90
-):
-    
-    start_date = datetime.strptime(start_date, "%Y-%m-%d")
-    demand = pd.DataFrame()
-    demand["date"] = pd.date_range(start_date, periods=time_periods, freq='D')
-
-    return demand
-
-generate_demand()
