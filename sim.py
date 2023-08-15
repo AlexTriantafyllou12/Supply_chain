8d5e8065999f9e320c6179ebe298efb31f4b54c9
Lina Rietuma <lina.rietuma@blend360.com>
2023-08-15 19:00:50 +0100

    Add simulation for the continous review policy SKUs

--- sim.py
+++ sim.py
@@ -1,26 +1,31 @@
 import funcs
 import random
 import numpy as np
-import json
+import configparser
+import pandas as pd
 
-# define the restrictions
-lead_time_mean  = [3, 5]
-lead_time_sd    = [2, 1]
+# import global varibale from a config file
+config = configparser.ConfigParser()
+config.read('sim.config')
+global_variables = config['global']
 
-nr_SKUs         = 1
-nr_sims         = 3
+lead_time_mean = np.array(list(global_variables['lead_time_mean']), dtype=int)
+lead_time_sd = np.array(list(global_variables['lead_time_sd']), dtype=int)
 
-demand_mean     = [random.randint(300, 600) for i in range(nr_SKUs)]
-demand_sd       = [random.randint(50, 100) for i in range(nr_SKUs)]
+nr_SKUs = int(global_variables['nr_SKUs'])
+nr_sims = int(global_variables['nr_sims'])
 
-holding_costs   = 0.05 # percent of the total cost of the item stored
-delivery_cost   = 100 # constant per delivery
-stock_out_cost  = 100000
-per_item_cost   = 100
-time_periods    = 20
-scrap_value     = 300
+holding_costs = float(global_variables['holding_costs']) # percent of the total cost of the item stored
+delivery_cost = int(global_variables['delivery_cost']) # constant per delivery
+stock_out_cost = int(global_variables['stock_out_cost'])
+per_item_cost = int(global_variables['per_item_cost'])
+time_periods = int(global_variables['time_periods'])
 
-sim_results     = {} # simulation results will go here
+# define demand mean/ sd based on the number of SKUs
+demand_mean = [random.randint(300, 600) for i in range(nr_SKUs)]
+demand_sd = [random.randint(50, 100) for i in range(nr_SKUs)]
+
+sim_results = {} # simulation results will go here
 
 # generate demand per SKU
 demand = funcs.generate_demand(
@@ -37,18 +42,14 @@ safety_stocks = np.asarray([funcs.find_safety_stock(
                           lead_time_sd=lead_time_sd[i%2]  
                         ) for i in range(nr_SKUs)])
 
-#print("SS: ", safety_stocks)
-
 rop = np.asarray([funcs.find_reorder_point(
                     demand_mean=demand_mean[i],
                     safety_stock=safety_stocks[i],
                     lead_time_mean=lead_time_mean[i%2]
                     ) for i in range(nr_SKUs)])
 
-print("ROP: ", rop)
-
 # assume starting inventory is 3 times the reorder point
-starting_inventory = np.asarray([rop[i] * 3 for i in range(nr_SKUs)])
+starting_inventory = np.asarray([rop[i] * 2 for i in range(nr_SKUs)])
 
 # sampling population for the periodic review policy
 periodic_review_periods = np.arange(1,90,1)
@@ -60,103 +61,54 @@ max_quantity = np.arange(4000, 20000, 500)
 contant_quantity = np.arange(500, 20000, 500)
 
 #for sample_inventory_policies:
- # SKU0 = periodic 5 day review, order up to a point
+ # SKU0 = periodic review, order up to a point
  # SKU1 = reorder once inventory levels reach reorder point (rop), constant quantity
  # SKU2 = reorder once inventory levels reach rop, order up to a point
  # SKU3 = reorder once inventory levels reach rop, constant quantity
- # SKU4 = periodic review, order up to 10000 units
+ # SKU4 = periodic review, constant quantity
 
-inventory = starting_inventory
-sku_0_review_days  = 2
-sku_4_max_quantity = 10000
+policy_types = ["max", "const", "max", "const", "max"]
 
-for s in range(nr_sims):
-    sim_results["s_" + str(s)] = {} # results for each simulation will go here
-    
-    # define the review policy for each SKU
-    max_quantity_SKU_0      = random.choice(max_quantity)
-    constant_quantity_SKU_1 = random.choice(contant_quantity)
-    max_quantity_SKU_2      = random.choice(max_quantity)
-    contant_quantity_SKU_3  = random.choice(contant_quantity)
-    periodic_review_SKU_4   = random.choice(periodic_review_periods)
+periodic_review_SKUs = [0, 4]
+continuous_review_SKUs = [1,2,3]
 
-    # delivery arrival time period and size, there might be several deliveries in the pipeline, hence a list is used
-    t_arrival_0 = []
-    replenishment_0 = []
+# simulate SKUs with a continous review policy
+for sku in range(len(continuous_review_SKUs)):
+    sku_index = continuous_review_SKUs[sku]
+    sim_results["SKU_"+str(continuous_review_SKUs[sku])] = {}
 
-    for t in range(time_periods):
-
-        sim_results["s_" + str(s)]["t_" + str(t)] = {} # results for each time period will go here    
-                
-        # SKU0
-        d_0 = 0
-        s_0 = 0
-        i_0 = 1
-        order_0 = 0 # order size 
-        current_inventory_0 = inventory[0]
-
-        print("Tarrival: {}, t: {}".format(t_arrival_0, t))
-        print("Replenishment ", replenishment_0)
-        # is it delivery day?
-        if (t_arrival_0[0] if len(t_arrival_0) != 0 else -1) == t:
-            current_inventory_0 = current_inventory_0 + replenishment_0[0]
-            # remove from the list 
-            t_arrival_0.pop(0)
-            replenishment_0.pop(0)
+    for s in range(nr_sims):
+        # random sampling of max and constant quantity values 
+        max_quantities = [random.choice(max_quantity) for i in range(len(continuous_review_SKUs))]
+        constant_quantities = [random.choice(contant_quantity) for i in range(len(continuous_review_SKUs))]
         
+        # run the simulation
+        output = funcs.inventory_sim(
+                    time_periods=time_periods,
+                    max_quantity=max_quantities[sku],
+                    constant_quantity=constant_quantities[sku],
+                    starting_inventory=starting_inventory[sku_index],
+                    rop=rop[sku_index],
+                    lead_time=lead_time_mean[sku_index%2],
+                    per_item_cost=per_item_cost,
+                    holding_costs=holding_costs,
+                    delivery_cost=delivery_cost,
+                    stock_out_cost=stock_out_cost,
+                    demand=demand[sku_index],
+                    policy_type=policy_types[sku_index])
         
-        
-        # is it review day?
-        if t%sku_0_review_days == 0:
-            # reorder if inventory below ROP
-            if current_inventory_0 < rop[0]:
-                # find order size
-                order_0 = max_quantity_SKU_0 - current_inventory_0
-                d_0 = 1 # indicate an order took place
-
-                t_arrival_0.append(t + lead_time_mean[0]) # order arrival time period
-                replenishment_0.append(order_0) 
-
-        # has there been a stock out?
-        if current_inventory_0 < 0:
-            s_0 = 1 # indicate there was a stock out
-            i_0 = 0 # indicate there's no stock
-
-        # find the cost 
-        cost_0 = funcs.cost_function(
-                            i=i_0, 
-                            inventory=current_inventory_0,
-                            per_item_cost=per_item_cost,
-                            holding_costs=holding_costs,
-                            d=d_0,
-                            order_size=order_0,
-                            delivery_cost=delivery_cost,
-                            s=s_0,
-                            stock_out_cost=stock_out_cost)
-        
-        # save the results 
-        # inventory is recorded at the beginning of the day (following a delivery (if any)) 
-        # demand_t will affect inventory_t+1
-        sim_results["s_" + str(s)]["t_" + str(t)]["SKU_0"] = {
-            "SKU": 0,
-            "Period": t,
-            "Simulation": s,
-            "Demand": demand[0][t],
-            "Inventory": current_inventory_0,
-            "Ordered": order_0,
-            "Cost": cost_0
-        }
-
-        # find next day's inventory
-        inventory[0] = current_inventory_0 - demand[0][t]
-
-        # SKU1
-
-        # SKU2
-
-        # SKU3
-
-        # SKU4
+        sim_results["SKU_"+str(continuous_review_SKUs[sku])]["s_"+str(s)] = output
 
 
-print(sim_results)
+df_output = pd.DataFrame()
+
+# convert the nested results dicionary into a dataframe
+for key in sim_results.keys():
+    for sub_key in sim_results[key].keys():
+
+        df =pd.DataFrame.from_dict(sim_results[key][sub_key],orient='index')
+        df["Simulation"] = sub_key
+        df["SKU"] = key
+        df_output = pd.concat([df_output, df])
+
+df_output.to_csv('data/sim_results.csv', index=False)
