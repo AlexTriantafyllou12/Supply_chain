0a1765afd3b2435bfdf37a4e312db32382d1edbf
Lina Rietuma <lina.rietuma@blend360.com>
2023-10-11 16:55:42 +0100

    Initial commit

---
+++ SolutionsGenerator.py
@@ -0,0 +1,7 @@
+class SolutionGenerator:
+    def __init__(self,
+                 nr_periods,
+                 nr_skus) -> None:
+        
+        self.nr_periods = nr_periods
+        self.nr_skus = nr_skus
