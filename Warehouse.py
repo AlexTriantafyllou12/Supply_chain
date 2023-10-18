9805a7fe2ac4f945d2518dc72ae2e36871677d33
Lina Rietuma <lina.rietuma@blend360.com>
2023-10-18 16:22:22 +0100

    Add a limitation on the proportion of warehouse an item can take up

--- Warehouse.py
+++ Warehouse.py
@@ -5,11 +5,11 @@ class Warehouse:
     def __init__(self,
                  max_capacity,
                  current_capacity,
-                 holding_cost) -> None:
+                 maintenance_cost) -> None:
         
         self.max_capacity = max_capacity
         self.current_capacity = current_capacity
-        self.holding_cost = holding_cost
+        self.maintenance_cost = maintenance_cost
 
     @u.check_integer_input
     def set_current_capacity(self, value) -> None:
@@ -23,6 +23,10 @@ class Warehouse:
         for i in range(nr_skus-1):
             # find the allocation per item
             allocated_q = random.randint(1, max_capacity - nr_skus + i - 1)
+            # ensure an item doesn't take up more than half the warehouse
+            while max_capacity/allocated_q < 0.3:
+                allocated_q = random.randint(1, max_capacity - nr_skus + i - 1)
+            
             # update the maximum capacity
             max_capacity = max_capacity - allocated_q
             allocations.append(allocated_q)
