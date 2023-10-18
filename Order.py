3bd934dbb36acb3e8efe27a3ac66bceffbe6b5a7
Lina Rietuma <lina.rietuma@blend360.com>
2023-10-18 11:54:55 +0100

    Include delivery_cost in class definition

--- Order.py
+++ Order.py
@@ -3,17 +3,19 @@ class Order:
                  SKUs,
                  quantity,
                  price,
+                 delivery_cost,
                  delivery_day) -> None:
         
         
         self.SKUs = SKUs
         self.quantity = quantity
         self.price = price
+        self.delivery_cost = delivery_cost
         self.delivery_day = delivery_day
 
     
     def find_order_price(self) -> float:
-        total_price = sum(self.price)
+        total_price = sum(self.price) + self.delivery_cost
 
         return total_price
 
