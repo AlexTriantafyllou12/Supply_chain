2b4fa4c06241eee558d1a9b6907faf5d8205ca97
Lina Rietuma <lina.rietuma@blend360.com>
2023-11-03 15:11:41 +0000

    Initial commit

---
+++ GA_core/policies.py
@@ -0,0 +1,57 @@
+class PolicyInterface:
+
+    def find_quantity_to_order(self) -> None:
+        pass
+
+    def order(self) -> None:
+        pass
+
+
+class MinMax(PolicyInterface):
+    """
+    continous policy with the variables - min value (i.e., rop) and max value (i.e., the order-up-to point)
+    """
+    
+    def find_quantity_to_order(self) -> None:
+        pass
+
+    def order(self) -> None:
+        pass
+
+
+class QR(PolicyInterface):
+    """
+    continous policy with the variables - quantity to order (Q) and reorder point (R)
+    """
+
+    def find_quantity_to_order(self) -> None:
+        pass
+
+    def order(self) -> None:
+        pass
+
+
+class Periodic_Up_To_Point(PolicyInterface):
+    """
+    periodic policy with variables - time periods and order-up-to value
+    """
+
+    def find_quantity_to_order(self) -> None:
+        pass
+
+    def order(self) -> None:
+        pass
+
+
+class Policy_Factory:
+
+    @staticmethod
+    def create_policy(type):
+        if type == 'minmax':
+            return MinMax()
+        elif type == 'qr':
+            return QR()
+        elif type == 'periodic_utp':
+            return Periodic_Up_To_Point()
+        
+        else: raise ValueError("nope...")
