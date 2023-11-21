b2c2c2ccb323696d84d3857a5d991c6ad220be5c
Lina Rietuma <lina.rietuma@blend360.com>
2023-11-21 09:27:55 +0000

    Code organisation

---
+++ src/supply_chain/solution.py
@@ -0,0 +1,46 @@
+import GA_core as ga_opt
+
+class Individual_Solution(ga_opt.Chromosome):
+    """A class representing an inidividual solution (i.e., a collection of policies)
+
+    Attributes:
+        skus (list): a list of SKU_Type objects 
+    """
+
+    def __init__(self) -> None:
+        """A constructor for the Individual_Solution class.
+
+        Args:
+            skus (list): a list of SKU_Type objects 
+        """
+        self.solution = []
+        pass
+        
+        
+    def solution_initialize(self,
+                            policyTypes,
+                            skus) -> None:
+
+        """Initialise the solution
+        """
+        pass
+
+    def solution_evaluation(self) -> float:
+
+        """Evaluate the fitness of the solution
+
+        Returns:
+            float: the fitness of the solution
+        """
+        pass
+
+    def mutate_chrom(self,
+               loc: int) -> None:
+        
+        """Mutates a policy in the solution
+
+        Args:
+            loc (int): location of the mutation
+        """
+
+        pass
